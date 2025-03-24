4089d01bfe9eec3f241178e533ffb22aae4900dc
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-03-24 10:28:55 -0300

    Update main.h

--- main.h
+++ main.h
@@ -1,1 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct format_handler {
+    char specifier;
+    int (*handler)(va_list);
+} format_t;
+
+int _printf(const char *format, ...);
+
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_integer(va_list args);
+int print_unsigned(va_list args);
+int print_octal(va_list args);
+int print_hex_lower(va_list args);
+int print_hex_upper(va_list args);
+int print_pointer(va_list args);
+
+int _putchar(char c);
+int print_number(long int n, int base, char *digits);
+
+#endif
