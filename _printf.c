09c466f0966ca4395215d0d969509d075ac5097a
854113 <9392@holbertonstudents.com>
2025-03-26 13:28:43 +0000

    task 0

--- _printf.c
+++ _printf.c
@@ -1,1 +1,56 @@
+#include "main.h"
 
+/**
+ * _printf - Produces output according to a format
+ * @format: Character string with zero or more directives
+ *
+ * Return: Number of characters printed (excluding null byte)
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+	int i = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+
+			switch (format[8])
+			{
+				case 'c':
+					count += print_char(args);
+					break;
+
+				case 's':
+					count += print_string(args);
+					break;
+
+				case '%':
+					count += _putchar('%');
+					break;
+				default:
+
+					count += _putchar('%');
+					count += _putchar(format[i]);
+			}
+		}
+		else
+		{
+			count += _putchar(format[i]);
+		}
+		i++;
+	}
+
+	va_end(args);
+
+	return (count);
+}
--- utils.c
+++ utils.c
@@ -1,1 +1,54 @@
+#include "main.h"
 
+
+/**
+ * _putchar - Writes a single character to stdout
+ * @c: The character to print
+ *
+ * Return: 1 on success, -1 on error
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_char - Prints a character from variable arguments
+ * @args: List of arguments
+ *
+ * Return: Number of characters printed
+ */
+
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	return (_putchar(c));
+}
+
+
+/**
+ * print_string - Prints a string from variable arguments
+ * @args: List of arguments
+ *
+ * Return: Number of characters printed
+ */
+
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int count = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (*str)
+	{
+		_putchar(*str);
+		count++;
+		str++;
+	}
+
+	return (count);
+}
