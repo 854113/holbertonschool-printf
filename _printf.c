47b0dc0c7885b30c74c79eacee22c428371ffd84
854113 <9392@holbertonstudents.com>
2025-03-27 13:08:26 +0000

    Update

--- _printf.c
+++ _printf.c
@@ -1,67 +1,43 @@
 #include "main.h"
 
 /**
- * handle_format_specifier - Handles individual format specifiers
- * @specifier: The format specifier character
- * @args: Variable argument list
- * @count: Pointer to count of printed characters
- *
- * Return: Number of characters added to count
- */
-
-int handle_format_specifier(char specifier, va_list args, int *count)
-{
-	int added = 0;
-
-	switch (specifier)
-	{
-		case 'c':
-			added = print_char(args);
-			break;
-		case 's':
-			added = print_string(args);
-			break;
-		case '%':
-			added = _putchar('%');
-			break;
-		default:
-			added = _putchar('%');
-			added += _putchar(specifier);
-			break;
-	}
-
-	*count += added;
-	return (added);
-}
-
-/**
  * _printf - Produces output according to a format
  * @format: Character string with zero or more directives
  *
- * Return: Number of characters printed (excluding null byte)
+ * Return: Number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0, i = 0;
+	int count = 0;
+	int i = 0;
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
 
-	while (format[i])
+	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == '\0')
+			switch (format[i])
 			{
-				va_end(args);
-				return (-1);
+				case 'c':
+					count += print_char(args);
+					break;
+				case 's':
+					count += print_string(args);
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				default:
+					count += _putchar('%');
+					count += _putchar(format[i]);
+					break;
 			}
-			handle_format_specifier(format[i], args, &count);
 		}
 		else
 		{
--- main.h
+++ main.h
@@ -2,31 +2,11 @@
 #define MAIN_H
 
 #include <stdarg.h>
-#include <stdlib.h>
 #include <unistd.h>
 
-/**
- * struct format_handler - Structure for format specifier handling
- * @specifier: Format specifier character
- * @handler: Function pointer to handle the specifier
- */
-typedef struct format_handler
-{
-	char specifier;
-	int (*handler)(va_list);
-} format_t;
-
 int _printf(const char *format, ...);
+int _putchar(char c);
 int print_char(va_list args);
 int print_string(va_list args);
-int print_percent(va_list args);
-int print_integer(va_list args);
-int print_unsigned(va_list args);
-int print_octal(va_list args);
-int print_hex_lower(va_list args);
-int print_hex_upper(va_list args);
-int print_pointer(va_list args);
-int _putchar(char c);
-int print_number(long int n, int base, char *digits);
 
 #endif /* MAIN_H */
