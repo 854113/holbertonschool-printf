5cdab61b67a20371d0105461df249b152c130b73
854113 <9392@holbertonstudents.com>
2025-03-31 13:45:36 +0000

    task 1

--- utils.c
+++ utils.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _putchar - Writes a single character to stdout
  * @c: The character to print
@@ -10,7 +9,6 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
 /**
  * print_char - Prints a character from variable arguments
  * @args: List of arguments
@@ -22,7 +20,6 @@ int print_char(va_list args)
 	char c = va_arg(args, int);
 	return (_putchar(c));
 }
-
 /**
  * print_string - Prints a string from variable arguments
  * @args: List of arguments
@@ -33,20 +30,16 @@ int print_string(va_list args)
 {
 	char *str = va_arg(args, char *);
 	int count = 0;
-
 	if (str == NULL)
 		str = "(null)";
-
 	while (*str)
 	{
 		_putchar(*str);
 		count++;
 		str++;
 	}
-
 	return (count);
 }
-
 /**
  * print_integer - Prints an integer from variable arguments
  * @args: List of arguments
@@ -57,34 +50,39 @@ int print_integer(va_list args)
 {
 	int num = va_arg(args, int);
 	int count = 0;
-	char buffer[10];
+	unsigned int n;
+	char buffer[12];
 	int i = 0;
 	int j;
-
+	
 	if (num == 0)
 	{
 		_putchar('0');
 		return (1);
 	}
-
+	
 	if (num < 0)
 	{
 		_putchar('-');
-		num = -num;
+		n = (unsigned int)(-num);
 		count++;
 	}
-
-	while (num > 0)
+	else
 	{
-		buffer[i++] = (num % 10) + '0';
-		num /= 10;
+		n = (unsigned int)num;
 	}
-
+	
+	while (n > 0)
+	{
+		buffer[i++] = (n % 10) + '0';
+		n /= 10;
+	}
+	
 	for (j = i - 1; j >= 0; j--)
 	{
 		_putchar(buffer[j]);
 		count++;
 	}
-
+	
 	return (count);
 }
